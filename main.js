68386d03bc40be9ea93342fbaa8c0efc7592d9d7
Boris Velkovski <bobi_velk@abv.bg>
2017-09-19 11:08:47 +0300

    Working Web App.

--- main.js
+++ main.js
@@ -9,11 +9,13 @@ app.use(bodyParser.json())
 app.get('/rest/endpoints/get-all', function(req, res){
 	createClient(function (err, client){
 		if(err){
+			res.status(500)
 			return res.send(err.message)
 		}
 
 	client.query("SELECT * FROM testtbl", function(err, resultSet){
 		if(err){
+			res.status(500)
 			return res.send("Could not fetch rows: "+ err.message)
 		}
 		res.json(resultSet.rows)
@@ -23,11 +25,13 @@ app.get('/rest/endpoints/get-all', function(req, res){
 app.post('/rest/endpoints/create', function(req, res){
 	createClient(function (err, client){
 		if(err){
+			res.status(500)
 			return res.send(err.message)
 		}
 		client.query("INSERT INTO testtbl (content) VALUES($1) RETURNING *", [req.body.content],
 				function(err, result){
 					if(err){
+						res.status(500)
 						return res.send('Could not insert entry into database: '+ err.message)
 					}
 					res.json(result.rows[0])
